1517d73e8af04a86b9cd14108c86f222d95ccddf
marronxd <aaron.burciaga262788@potros.itson.edu.mx>
2026-02-15 14:53:07 -0700

    ScriptBase de datos pizzeria v1.
    Primer avance sin la entidad cupon. Script para crear la base de datos

---
+++ AvancesSQL/scriptPizzeria_Proyecto.sql
@@ -0,0 +1,77 @@
+-- Creación de base de datos
+-- creadores: los chiquibases
+drop database if exists pizzeria;
+create database pizzeria;
+use pizzeria;
+
+-- creacion de tablas 
+create table domicilios_clientes (
+	id_domicilio int primary key auto_increment,
+    calle varchar(100) not null,
+    numero varchar(30) not null,
+    colonia varchar(150) not null,
+    codigo_postal varchar(40) not null
+);
+
+create table clientes(
+	id_cliente int primary key,
+    nombres varchar(80) not null,
+    apellido_paterno varchar(60) not null,
+    apellido_materno varchar(60) null,
+    fecha_nacimiento datetime not null,
+    id_domicilio int not null, -- Llave foranea de domicilios
+    foreign key (id_domicilio) references domicilios_clientes (id_domicilio)
+    on delete cascade
+    on update cascade
+);
+
+create table pedidos(
+	id_pedido int primary key auto_increment,
+    pin varchar(255) not null, 
+    tipo varchar(60) not null,
+    estado enum("Listo","No reclamado", "Entregado", "Cancelado", "En preparacion")  not null,
+    folio varchar(36),
+    fechaHora_entrega datetime null,
+    fechaHora_elaboracion datetime null,
+    fechaHora_descarte datetime null,
+    id_cliente int not null,
+    foreign key (id_cliente) references clientes(id_cliente)
+    on delete cascade
+    on update cascade
+);
+
+create table pizzas_catalogo(
+	id_pizza_catalogo int primary key auto_increment,
+    nombre varchar(100) not null,
+    tamaño enum("Chica","Grande","Mediana") not null,
+    descripcion varchar(250) not null,
+    precio int not null,
+    estado enum("Disponible", "No disponible") not null default "Disponible"
+);
+
+create table pizzas(
+	id_pizza int primary key auto_increment,
+    costo int not null,
+	notas varchar(500) not null,
+    id_pizza_catalogo int not null,
+    foreign key (id_pizza_catalogo) references pizzas_catalogo(id_pizza_catalogo)
+    on delete cascade
+    on update cascade
+);
+
+create table ingredientes(
+	id_ingrediente int primary key auto_increment,
+    descripcion varchar(200),
+    nombre varchar(200)
+);
+create table ingrediente_pizzaCatalog(
+	id int primary key auto_increment,
+    id_pizza_catalogo int not null,
+    id_ingrediente int not null,
+    foreign key (id_pizza_catalogo) references pizzas_catalogo(id_pizza_catalogo)
+    on delete cascade
+    on update cascade,
+    foreign key (id_ingrediente) references ingredientes(id_ingrediente)
+);
+
+
