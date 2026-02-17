844cce4d1a8d463ab58a44cdcb53de7704d5710e
marronxd <aaron.burciaga262788@potros.itson.edu.mx>
2026-02-16 20:53:48 -0700

    ScriptSQL_pizzeria_v3
    A algunos atributos se le modificaron si puede ser null o no. fechaEmision de cupon se eliminó. Se agregó un registro de los cambios realizados a los estados.

--- AvancesSQL/scriptPizzeria_Proyecto.sql
+++ AvancesSQL/scriptPizzeria_Proyecto.sql
@@ -27,15 +27,28 @@ create table clientes(
 
 create table pedidos(
 	id_pedido int primary key auto_increment,
-    pin varchar(255) not null, 
-    tipo varchar(60) not null,
+    pin varchar(255) null, 
+    tipo varchar(60) null,
     estado enum("Listo","No reclamado", "Entregado", "Cancelado", "En preparacion")  not null,
-    folio varchar(36),
+    folio varchar(36) null,
     fechaHora_entrega datetime null,
-    fechaHora_elaboracion datetime null,
+    fechaHora_elaboracion datetime not null,
     fechaHora_descarte datetime null,
     id_cliente int not null,
     foreign key (id_cliente) references clientes(id_cliente)
+    on delete cascade
+    on update cascade
+);
+
+-- Tabla que guarda el historial de esos cambios de estado de cada pedid
+
+create table historial_cambios_estado_pedidos(
+	id int primary key auto_increment,
+    id_pedido int not null,
+    estado_anterior enum("Listo","No reclamado", "Entregado", "Cancelado", "En preparacion") not null,
+    estado_actual enum("Listo","No reclamado", "Entregado", "Cancelado", "En preparacion") null,
+    fechaHora_actualizacion datetime null,
+    foreign key (id_pedido) references pedidos(id_pedido)
     on delete cascade
     on update cascade
 );
@@ -45,13 +58,13 @@ create table pizzas_catalogo(
     nombre varchar(100) not null,
     tamaño enum("Chica","Grande","Mediana") not null,
     descripcion varchar(250) not null,
-    precio int not null,
+    precio double(8,2) not null,
     estado enum("Disponible", "No disponible") not null default "Disponible"
 );
 
 create table pizzas(
 	id_pizza int primary key auto_increment,
-    costo int not null,
+    costo double(8,2) not null,
 	notas varchar(500) not null,
     id_pizza_catalogo int not null,
     foreign key (id_pizza_catalogo) references pizzas_catalogo(id_pizza_catalogo)
@@ -64,7 +77,8 @@ create table ingredientes(
     descripcion varchar(200),
     nombre varchar(200)
 );
-create table ingrediente_pizzaCatalog(
+
+create table ingrediente_pizza_catalogo(
 	id int primary key auto_increment,
     id_pizza_catalogo int not null,
     id_ingrediente int not null,
@@ -78,14 +92,13 @@ create table ingrediente_pizzaCatalog(
 
 create table cupones(
 	id_cupon int primary key auto_increment,
-	fecha_emision datetime not null,
-    usos int not null,
-    fecha_fin datetime not null,
-    fecha_emision datetime null -- a la mejor en promocion el cupon pero está guardado
+    limite_usos int not null,
+    fecha_caducidad datetime not null
+    -- fecha_emision datetime null -- a la mejor en promocion el cupon pero está guardado
 );
 
 create table cupones_pedidos(
-	id_cuponePed int primary key auto_increment,
+	id_cupon_pedido int primary key auto_increment,
     id_cupon int not null,
     id_pedido int not null,
     foreign key (id_cupon) references cupones(id_cupon)
