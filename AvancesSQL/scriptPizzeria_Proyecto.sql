bdaa05ef2888a62d8a7bfcc4a74d25faa584d24f
marronxd <aaron.burciaga262788@potros.itson.edu.mx>
2026-02-18 16:19:27 -0700

    ScriptSQL_pizzeria_v4
    Avance numero 4. Correcion del diagrama mer. se añadió 2 tablas para pedidos express y programados. Especializacion de usuario, ahora hay cliente y empleado y se eliminaron algunas tablas. ademas de modificaciones en atributos

--- AvancesSQL/scriptPizzeria_Proyecto.sql
+++ AvancesSQL/scriptPizzeria_Proyecto.sql
@@ -5,6 +5,17 @@ create database pizzeria;
 use pizzeria;
 
 -- creacion de tablas 
+create table usuarios(
+	id_usuario int primary key auto_increment,
+    nombreUsuario varchar(100) not null,
+    contraseña varchar(100) not null,
+    edad int not null,
+    tipo enum("cliente", "empleado") not null,
+    nombres varchar(80) not null,
+    apellido_paterno varchar(60) not null,
+    apellido_materno varchar(60) null
+);
+
 create table domicilios_clientes (
 	id_domicilio int primary key auto_increment,
     calle varchar(100) not null,
@@ -14,32 +25,79 @@ create table domicilios_clientes (
 );
 
 create table clientes(
-	id_cliente int primary key,
-    nombres varchar(80) not null,
-    apellido_paterno varchar(60) not null,
-    apellido_materno varchar(60) null,
+	id_usuario int primary key,
+    estado enum("activo", "inactivo") default "activo",
+    correo varchar(150) not null,
     fecha_nacimiento datetime not null,
-    id_domicilio int not null, -- Llave foranea de domicilios
+    id_domicilio int null, -- Llave foranea de domicilios
     foreign key (id_domicilio) references domicilios_clientes (id_domicilio)
     on delete cascade
-    on update cascade
-);
-
-create table pedidos(
-	id_pedido int primary key auto_increment,
-    pin varchar(255) null, 
-    tipo varchar(60) null,
-    estado enum("Listo","No reclamado", "Entregado", "Cancelado", "En preparacion")  not null,
-    folio varchar(36) null,
-    fechaHora_entrega datetime null,
-    fechaHora_elaboracion datetime not null,
-    fechaHora_descarte datetime null,
-    id_cliente int not null,
-    foreign key (id_cliente) references clientes(id_cliente)
+    on update cascade,
+    foreign key (id_usuario) references usuarios(id_usuario)
     on delete cascade
     on update cascade
 );
 
+create table telefonos_clientes(
+	id_telefono int primary key,
+    id_usuario int not null,
+    telefono varchar(10) not null,
+    etiqueta varchar(100) not null,
+    foreign key (id_usuario) references clientes(id_usuario)
+	on delete cascade
+    on update cascade
+);
+
+create table empleados(
+	id_usuario int primary key,
+    telefono varchar(10) not null,
+    rol enum("Cocinero", "Cajero") not null,
+    foreign key (id_usuario) references usuarios(id_usuario)
+    on delete cascade
+    on update cascade
+);
+create table pedidos(
+	id_pedido int primary key auto_increment,
+    metodo_pago enum("Efectivo", "Tarjeta") not null,
+    total double(8,2) not null,
+    totalDCTO double(8,2) null,
+    tipo varchar(60) null,
+    estado enum("Listo","No reclamado", "Entregado", "Cancelado", "En preparacion")  not null default "En preparacion",
+    fechaHora_entrega datetime null,
+    fechaHora_elaboracion datetime not null,
+    id_usuario int null,
+    foreign key (id_usuario) references clientes(id_usuario)
+    on delete cascade
+    on update cascade
+);
+
+create table pedidos_express(
+	id_pedido int primary key,
+	pin varchar(255) not null, 
+	folio varchar(36) not null,
+    foreign key (id_pedido) references pedidos(id_pedido)
+    on update cascade
+    on delete cascade
+);
+
+create table cupones(
+	id_cupon int primary key auto_increment,
+    limite_usos int null,
+    fecha_caducidad datetime null,
+    fecha_emision datetime null,
+	codigo varchar(50) unique not null
+);
+
+create table pedidos_programados(
+	id_pedido int primary key,
+    id_cupon int null,
+    foreign key (id_pedido) references pedidos(id_pedido)
+    on update cascade
+    on delete cascade,
+	foreign key (id_cupon) references cupones(id_cupon)
+    on update cascade
+    on delete cascade
+);
 -- Tabla que guarda el historial de esos cambios de estado de cada pedid
 
 create table historial_cambios_estado_pedidos(
@@ -53,36 +111,38 @@ create table historial_cambios_estado_pedidos(
     on update cascade
 );
 
-create table pizzas_catalogo(
-	id_pizza_catalogo int primary key auto_increment,
-    nombre varchar(100) not null,
+-- continua el flujo normal
+create table pizzas(
+	id_pizza int primary key auto_increment,
+	nombre varchar(100) not null,
     tamaño enum("Chica","Grande","Mediana") not null,
     descripcion varchar(250) not null,
     precio double(8,2) not null,
     estado enum("Disponible", "No disponible") not null default "Disponible"
 );
 
-create table pizzas(
-	id_pizza int primary key auto_increment,
+create table detallesPizzas(
+	id int primary key auto_increment,
     costo double(8,2) not null,
-	notas varchar(500) not null,
-    id_pizza_catalogo int not null,
-    foreign key (id_pizza_catalogo) references pizzas_catalogo(id_pizza_catalogo)
+    cantidad int not null,
+	notas varchar(500) null,
+    id_pizza int not null,
+    foreign key (id_pizza) references pizzas(id_pizza)
     on delete cascade
     on update cascade
 );
 
 create table ingredientes(
 	id_ingrediente int primary key auto_increment,
-    descripcion varchar(200),
-    nombre varchar(200)
+    descripcion varchar(200) not null,
+    nombre varchar(200) not null
 );
 
-create table ingrediente_pizza_catalogo(
+create table ingredientes_pizzas(
 	id int primary key auto_increment,
-    id_pizza_catalogo int not null,
-    id_ingrediente int not null,
-    foreign key (id_pizza_catalogo) references pizzas_catalogo(id_pizza_catalogo)
+    id_pizza int not null,
+    id_ingrediente int null,
+    foreign key (id_pizza) references pizzas(id_pizza)
     on delete cascade
     on update cascade,
     foreign key (id_ingrediente) references ingredientes(id_ingrediente)
@@ -90,21 +150,3 @@ create table ingrediente_pizza_catalogo(
     on update cascade
 );
 
-create table cupones(
-	id_cupon int primary key auto_increment,
-    limite_usos int not null,
-    fecha_caducidad datetime not null
-    -- fecha_emision datetime null -- a la mejor en promocion el cupon pero está guardado
-);
-
-create table cupones_pedidos(
-	id_cupon_pedido int primary key auto_increment,
-    id_cupon int not null,
-    id_pedido int not null,
-    foreign key (id_cupon) references cupones(id_cupon)
-    on delete cascade
-    on update cascade,
-    foreign key (id_pedido) references pedidos(id_pedido)
-    on delete cascade
-    on update cascade
-);
